e4983cbb1d1fadcf57b3df139a9c164a366acdc0
fvntr <phiven@gmail.com>
2015-02-09 10:36:08 -0800

    add file

---
+++ Easy/LetterCount.js
@@ -0,0 +1,30 @@
+function ArrayAdditionI(arr) { 
+  
+  var greatest = arr.sort(function(a,b){return b-a}).shift();
+  var sum = 0;
+  
+  for(var i=0; i<arr.length; i++){
+    sum +=arr[i]
+      for(var j=0; j<arr.length; j++){
+        if(i !== j){
+           sum += arr[j]
+             if(sum == greatest){
+               return true
+             }
+        }
+      }
+    for(var k=0;k<arr.length;k++){
+      if(i !== k){
+        sum -= arr[k]
+          if(sum == greatest){
+            return true
+          }
+      }
+    }
+    sum = 0
+  }
+  
+  return false
+  
+}
+					
